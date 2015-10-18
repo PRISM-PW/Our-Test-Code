4df42860eab449a61ae83a89516047d5faca27e7
MyNameisSU <admin@prism.pw>
2015-10-17 21:00:10 -0400

    Create MyClass.h

---
+++ MyClass.h
@@ -0,0 +1,11 @@
+#ifndef MYCLASS_H_INCLUDED // Part 1 of Including the Source File
+#define MYCLASS_H_INCLUDED // Part 2 of Including the Source File
+
+class MyClass // Begins Class
+{ // Starts Container
+    public: Defines Class Access Policy
+        MyClass(); //Links to the Actual Source File
+}; \\ Ends Container
+
+
+#endif // MYCLASS_H_INCLUDED // Part 3 of Including the Source File
